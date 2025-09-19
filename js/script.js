7ed669193b4439f2b6c29728a6b0f94f7da23948
Ushus844 <107358689+Ushus844@users.noreply.github.com>
2025-09-19 16:33:43 +0500

    .
    .

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("Ushus")
+/*
+document.getElementById("redbox").style.backgroundColor = "red"
+*/
+console.log(document.querySelectorAll(".box"))
+
+document.querySelectorAll(".box").forEach(e =>
+    {
+e.style.backgroundColor = "green"
+
+    }
+)
